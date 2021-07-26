c89ea43000a83713efbef0f1457a3759ae04a802
中平 慶春 <nakadaira@nakahiranoMacBook-Pro-2.local>
2021-07-26 20:18:19 +0900

    todaydateの修正

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -30,17 +30,18 @@ class CalendarsController < ApplicationController
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
+      today_date = @todays_date + x
       today_plans = []
       plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
+        today_plans.push(plan.plan) if plan.date == today_date
       end
 
-      wday_num = Date.today.wday + x
+      wday_num = @todays_date.wday + x
       if wday_num >= 7
         wday_num = wday_num -7
       end
 
-      days = { month: (@todays_date + x).month, date: (@todays_date+x).day, plans: today_plans, wdays:wdays[wday_num] }
+      days = { month: (today_date).month, date: (today_date).day, plans: today_plans, wdays:wdays[wday_num] }
       @week_days.push(days)
     end
 
